3b3bfa55823baf13e1977ed08c52a9a13d9a652b
ADAS-team <engr.shamoon@gmail.com>
2019-02-20 18:57:33 -0500

    Initial Commit

---
+++ GUI.py
@@ -0,0 +1,133 @@
+import numpy as np
+import cv2
+import zmq
+import _thread
+from threading import Lock
+
+mutex = Lock()
+
+pxpy = []
+rectangles = []
+
+
+cap = cv2.VideoCapture(4)
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to fusion engine")
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5558")
+
+print("Entering GUI Loop...")
+
+def comms_thread():
+	while True:
+		message = socket.recv_string()
+		
+		pxpy.clear()
+		rectangles.clear()
+		
+		mutex.acquire()
+		
+		data = message.split('/')
+		camera_data = data[0]
+		radar_data = data[1]
+		lidar_data = data[2]
+		socket.send(b"world")
+			
+		camera_data_array = camera_data.split('|')
+		for xyc in camera_data_array:
+			rectangles.append(xyc.split('%'))
+			
+		radar_data_array = radar_data.split('|')
+		for uv in radar_data_array:
+			pxpy.append(uv.split('%'))
+			
+		print(camera_data)
+		print(rectangles)
+			
+		mutex.release()
+		
+'''	
+try: 
+	_thread.start_new_thread(comms_thread, ())
+	pass
+except:
+	print("Error: Unable to start thread")	
+'''
+	
+while(True):
+	
+	
+	message = socket.recv_string()
+	data = message.split('/')
+	camera_data = data[0]
+	radar_data = data[1]
+	lidar_data = data[2]
+	#print(data)
+	print(camera_data)
+	print(radar_data)
+	print(lidar_data)
+	print("-----------------------")
+	socket.send(b"world")
+	ret, frame = cap.read()
+	
+	modf_frame = frame
+	
+	
+	
+	
+	#breakdown the aggregated camera_data into individual rectangles, then draw rectangles on the frame. 
+	while len(camera_data)>20:
+		px1 = int(camera_data[0:4])
+		py1 = int(camera_data[4:8])
+		px2 = int(camera_data[8:12])
+		py2 = int(camera_data[12:16])
+		camera_certainty = float(camera_data[16:21])
+		camera_data = camera_data[21:]
+		modf_frame = cv2.rectangle(modf_frame, (px1, py1), (px2, py2), (0,255,0), 2)
+	
+	#breakdown the aggregated camera_data into individual rectangles, then draw rectangles on the frame. 
+	while len(radar_data)>7:
+		px = int(radar_data[0:4])
+		py = int(radar_data[4:8])
+		radar_data = radar_data[8:]
+		modf_frame = cv2.rectangle(modf_frame, (px, py), (px+10, py+10), (0,0,255), 2)
+	
+	'''
+	radar_data_array = radar_data.split('|')
+	print(radar_data_array)
+	for uv in radar_data_array:
+		print(uv)
+		pxpy = uv.split('%')
+		print(pxpy)
+		if len(pxpy)==2:
+			modf_frame = cv2.rectangle(modf_frame, (int(pxpy[0]), int(pxpy[1])), (int(pxpy[0])+10, int(pxpy[1])+10), (0,0,255), 2)
+'''
+	
+	'''
+	ret, frame = cap.read()
+	
+	modf_frame = frame
+	
+	mutex.acquire()
+	
+	for item in pxpy:
+		if(len(item)==2):
+			modf_frame = cv2.rectangle(modf_frame, (int(item[0]), int(item[1])), (int(item[0])+10, int(item[1])+10), (0,0,255), 2)
+			
+	for item in rectangles:
+		if(len(item) == 5):
+			modf_frame = cv2.rectangle(modf_frame, (item[0], item[1]), (item[2], item[3]), (0,255,0), 2)
+	
+	mutex.release()
+	
+	'''
+	
+	
+	cv2.imshow('frame',modf_frame)
+	
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	
---
+++ fusion.py
@@ -0,0 +1,242 @@
+import time
+import zmq
+import subprocess
+import _thread
+import datetime
+from threading import Lock
+
+import numpy as np
+import math
+import mkmath as mk
+import cv2
+
+PORT_CAMERA = "5555"
+PORT_LIDAR = "5556"
+PORT_RADAR = "5557"
+PORT_GUI = "5558"
+
+radar_data = list()
+camera_data = list()
+lidar_data = list()
+
+camera_data_mutex = Lock()
+radar_data_mutex = Lock()
+lidar_data_mutex = Lock()
+
+turn = True
+
+#Radar Configuration
+#RT=np.matrix('1 0 0 0;0 0 1 0;0 1 0 0;0 0 0 1') #based on corrected axes #discounted
+#KEEP FOR LIDAR
+#RT=np.matrix('0 1 0 0;0 0 1 0.05;1 0 0 -0.06')
+#Radar 
+RT=np.matrix('1 0 0 0;0 0 -1 -0.06;0 1 0 -0.05')
+
+fy=1040
+fx=720
+cx=640
+cy=360
+Sx=16/9
+Sy=1
+
+
+#pixel buffer for radar
+radar_pixel=[]
+
+
+
+def thread_camera():
+	
+	global turn
+	
+	while True:
+		
+		
+		#  Wait for next request from client
+		message_camera = socket_camera.recv()
+		#parse camera data
+		if (~turn):
+			camera_data_mutex.acquire()
+			camera_data.clear()
+			while len(message_camera)>19:
+				px1 = int(message_camera[0:4])
+				py1 = int(message_camera[4:8])
+				px2 = int(message_camera[8:12])
+				py2 = int(message_camera[12:16])
+				camera_certainty = float(message_camera[16:20])/10000
+				camera_data.append((px1, py1, px2, py2, camera_certainty))
+				message_camera = message_camera[20:]
+		
+			
+			while (len(camera_data)>=10):
+				del camera_data[0]	
+				
+			socket_camera.send(b"Ack_Cam")
+			
+			camera_data_mutex.release()
+			turn=True
+	
+def thread_radar():
+	while(True):  
+	#Blocking receive
+		message = socket_radar.recv()
+		socket_radar.send(b"200")
+		
+		radar_data.clear()
+		
+		#calculation of loop params	
+		number_of_obj= (len(message)-1)/15
+		#print("Number of detected Objects:")
+		#print(number_of_obj)
+		#counters	
+		count=0
+		#position on string
+		index=0 
+	
+		while(count<number_of_obj):
+			
+			#print("Entered loop no",count)
+			x = int(message[index:index+4+1])/100
+			y = int(message[index+5:index+9+1])/100
+			z = int(message[index+10:index+14+1])/100
+			
+			#calculating radius for scaling circle radius
+			r = math.sqrt(math.pow(x,2)+math.pow(y,2)+pow(z,2))
+			index = index+15
+			count = count+1
+			
+			#radius
+			rad=10
+			if(r<1):
+				rad=20
+			elif(r>10):
+				rad=5
+			else:
+				rad=5+15/9*r
+			
+			#pixel conversion===========================
+			
+			#old method
+			#uv= mk.convertWorldCordsToPixelsRadar(x,y,z,fx,fy,cx,cy,RT,Sx,Sy)
+			
+			#new method
+			uv=mk.LidToPixels(x,y,z,RT,fx,fy,Sx,Sy,cx,cy)
+			
+			radar_data.append(uv)
+			
+		#resetting loop params	
+		index=0
+		count=0
+		
+def thread_lidar():
+		message = socket_lidar.recv()
+		socket_lidar.send(b"200")
+		lidar_data.clear()
+		
+	
+
+#############################################
+########## INITALIZATION ###################
+###########################################
+
+#create camera vision socket
+context_camera = zmq.Context()
+socket_camera = context_camera.socket(zmq.REP)
+socket_camera.bind("tcp://*:" + PORT_CAMERA)
+
+#create radar socket
+context_radar = zmq.Context()
+socket_radar = context_radar.socket(zmq.REP)
+socket_radar.bind("tcp://*:" + PORT_RADAR)
+
+
+
+print("Initializing Camera Feed...")
+pid_camera = subprocess.Popen(["/home/nvidia/jetson-inference/jetson-inference/build/aarch64/bin//detectnet-camera", "pednet"])
+print("Configuring Radar to start producing data...")
+pid_radar_config = subprocess.Popen(["/home/nvidia/Documents/radar_cfgs/reader_writer"])
+print("Starting Radar incoming data parser ")
+pid_radar_data = subprocess.Popen(["/home/nvidia/Documents/radar_cfgs/dataport_reader_zmq"])
+print("Starting GUI...")
+pid_GUI = subprocess.Popen(["python3", "/home/nvidia/Documents/ADAS/GUI.py"])
+
+#create GUI socket
+context_gui = zmq.Context()
+socket_gui = context_gui.socket(zmq.REQ)
+socket_gui.connect("tcp://localhost:" + PORT_GUI)	
+
+#############################################
+########## END INITALIZATION ###################
+###########################################
+
+
+try: 
+	_thread.start_new_thread(thread_camera, ())
+	_thread.start_new_thread(thread_radar, ())
+	#_thread.start_new_thread(thread_lidar, ())
+except:
+	print("Error: Unable to start thread")
+	
+print("Entering loop")
+
+while 1:
+	
+	if(turn):
+		camera_data_mutex.acquire()
+		camera_buffer = ""
+		for x1, y1, x2, y2, cert in camera_data:
+			#camera_buffer += "{0:04d}%{1:04d}%{2:04d}%{3:04d}%{4:5.3f}|".format(x1, y1, x2, y2, cert)
+			camera_buffer += "{0:04d}{1:04d}{2:04d}{3:04d}{4:5.3f}".format(x1, y1, x2, y2, cert)
+			#cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3) 
+			#print(x1, y1, x2, y2, e)
+		camera_buffer = camera_buffer[:-1]
+		
+		#print("Radar Data")
+		radar_buffer = ""
+		#FIX THIS: radar_data is not in x,y,z
+		#for x, y, z in radar_data:
+		for u, v, w in radar_data:
+			#radar_buffer += "{0:03d}{1:03d}{2:03d}".format(x, y, z)
+			#radar_buffer += "{0:d}%{1:d}|".format(u, v)
+			#print(u)
+			#print(v)
+			if (u <=1280 and u>=0 and v<=720 and v>=0):
+				radar_buffer += "{0:04d}{1:04d}".format(u, v)
+			#cv2.circle(frame, (x, y), 40, (0, 0, 255), 3)
+			#print(x, y, z)
+			#fusion algorithm
+		
+		#radar_buffer = radar_buffer[:-1]
+		
+			
+		#print("==========")
+		
+		#crafting a single message to send to GUI, using '/' delimiters.
+		fusion_message = "{}/{}/{}".format(camera_buffer, radar_buffer, "c")
+		socket_gui.send_string(fusion_message)
+		gui_message = socket_gui.recv()
+		#print("Received reply")
+		
+		
+		camera_data_mutex.release()
+		
+		turn = False
+	
+	
+	pass
+
+
+
+
+
+
+#########################################
+############ HELPER FUNCTIONS ###########
+#########################################
+
+
+
+
+
+ 
+    
