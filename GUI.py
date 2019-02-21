a285657028665716d3b109645ff65080765993f5
adamfill28 <33565735+adamfill28@users.noreply.github.com>
2019-02-21 13:26:18 -0500

    Initial Commit

--- GUI.py
+++ GUI.py
@@ -5,10 +5,13 @@ import _thread
 from threading import Lock
 
 mutex = Lock()
+new_camera = False
+new_radar = False
+new_lidar = False
 
-pxpy = []
-rectangles = []
-
+detections_r = []
+detections_c = []
+detections_l = []
 
 cap = cv2.VideoCapture(1)
 
@@ -22,11 +25,16 @@ socket.bind("tcp://*:5558")
 print("Entering GUI Loop...")
 
 def comms_thread():
+	global new_camera, new_radar, new_lidar, detections_c, detections_l, detections_r, mutex
+
+
 	while True:
 		message = socket.recv_string()
+		socket.send(b"world")
 		
-		pxpy.clear()
-		rectangles.clear()
+		detections_r.clear()
+		detections_c.clear()
+		detections_l.clear()
 		
 		mutex.acquire()
 		
@@ -34,107 +42,65 @@ def comms_thread():
 		camera_data = data[0]
 		radar_data = data[1]
 		lidar_data = data[2]
-		socket.send(b"world")
+
+			#breakdown the aggregated camera_data into individual rectangles, then draw rectangles on the frame. 
+		while len(camera_data)>20:
+			px1 = int(camera_data[0:4])
+			py1 = int(camera_data[4:8])
+			px2 = int(camera_data[8:12])
+			py2 = int(camera_data[12:16])
+			#camera_certainty = float(camera_data[16:21])
+			camera_data = camera_data[21:]
+			detections_c.append([px1, py1, px2, py2])
+			new_camera = True
 			
-		camera_data_array = camera_data.split('|')
-		for xyc in camera_data_array:
-			rectangles.append(xyc.split('%'))
+		#breakdown the aggregated camera_data into individual rectangles, then draw rectangles on the frame. 
+		while len(radar_data)>7:
+			px = int(radar_data[0:4])
+			py = int(radar_data[4:8])
+			radar_data = radar_data[8:]
+			detections_c.append([px, py])
+			new_radar = True
 			
-		radar_data_array = radar_data.split('|')
-		for uv in radar_data_array:
-			pxpy.append(uv.split('%'))
-			
-		print(camera_data)
-		print(rectangles)
+		#lidar	
+		while len(lidar_data)>7:
+			px = int(lidar_data[0:4])
+			py = int(lidar_data[4:8])
+			circle_radius=int(lidar_data[8:12])
+			lidar_data = lidar_data[12:]
+			detections_l.append([px, py, circle_radius])
+			new_lidar = True
 			
 		mutex.release()
 		
-'''	
 try: 
 	_thread.start_new_thread(comms_thread, ())
-	pass
 except:
 	print("Error: Unable to start thread")	
-'''
-	
-while(True):
-	
-	
-	message = socket.recv_string()
-	data = message.split('/')
-	camera_data = data[0]
-	radar_data = data[1]
-	lidar_data = data[2]
-	#print(data)
-	print(camera_data)
-	print(radar_data)
-	print(lidar_data)
-	print("-----------------------")
-	socket.send(b"world")
-	ret, frame = cap.read()
-	
-	modf_frame = frame
-	
-	
-	
-	
-	#breakdown the aggregated camera_data into individual rectangles, then draw rectangles on the frame. 
-	while len(camera_data)>20:
-		px1 = int(camera_data[0:4])
-		py1 = int(camera_data[4:8])
-		px2 = int(camera_data[8:12])
-		py2 = int(camera_data[12:16])
-		camera_certainty = float(camera_data[16:21])
-		camera_data = camera_data[21:]
-		modf_frame = cv2.rectangle(modf_frame, (px1, py1), (px2, py2), (0,255,0), 2)
-	
-	#breakdown the aggregated camera_data into individual rectangles, then draw rectangles on the frame. 
-	while len(radar_data)>7:
-		px = int(radar_data[0:4])
-		py = int(radar_data[4:8])
-		radar_data = radar_data[8:]
-		modf_frame = cv2.rectangle(modf_frame, (px, py), (px+10, py+10), (0,0,255), 2)
-	
-	#lidar	
-	while len(lidar_data)>7:
-		px = int(lidar_data[0:4])
-		py = int(lidar_data[4:8])
-		circle_radius=int(lidar_data[8:12])
-		lidar_data = lidar_data[12:]
-		modf_frame=cv2.circle(modf_frame,(px, py),circle_radius,(0, 191, 255),1)
 
-	
-	'''
-	radar_data_array = radar_data.split('|')
-	print(radar_data_array)
-	for uv in radar_data_array:
-		print(uv)
-		pxpy = uv.split('%')
-		print(pxpy)
-		if len(pxpy)==2:
-			modf_frame = cv2.rectangle(modf_frame, (int(pxpy[0]), int(pxpy[1])), (int(pxpy[0])+10, int(pxpy[1])+10), (0,0,255), 2)
-'''
-	
-	'''
+while(True):
 	ret, frame = cap.read()
-	
 	modf_frame = frame
 	
 	mutex.acquire()
+
+	if(new_camera):
+		for i in detections_c:
+			modf_frame = cv2.rectangle(modf_frame, (i[0], i[1]), (i[2], i[3]), (0,255,0), 2)
+		new_camera = False
 	
-	for item in pxpy:
-		if(len(item)==2):
-			modf_frame = cv2.rectangle(modf_frame, (int(item[0]), int(item[1])), (int(item[0])+10, int(item[1])+10), (0,0,255), 2)
-			
-	for item in rectangles:
-		if(len(item) == 5):
-			modf_frame = cv2.rectangle(modf_frame, (item[0], item[1]), (item[2], item[3]), (0,255,0), 2)
-	
+	if(new_radar):
+		for i in detections_r:
+			modf_frame = cv2.rectangle(modf_frame, (i[0], i[1]), (i[0]+10, i[0]+10), (0,0,255), 2)
+		new_radar = False
+
+	if(new_lidar):
+		for i in detections_l:
+			modf_frame=cv2.circle(modf_frame,(i[0], i[1]), i[2],(0, 191, 255),1)
+		new_lidar = False
+		
 	mutex.release()
-	
-	'''
-	
-	
+
 	cv2.imshow('frame',modf_frame)
 	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
