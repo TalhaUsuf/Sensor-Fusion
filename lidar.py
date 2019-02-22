28176775a5d9bf1a5a1f19f46a049a6092ad50d3
ADAS-team <engr.shamoon@gmail.com>
2019-02-21 19:10:44 -0500

    KMeans clustering for the LIDAR

--- lidar.py
+++ lidar.py
@@ -2,6 +2,8 @@
 from rplidar import RPLidar
 import zmq
 import _pickle as pickle
+import numpy as np  
+from sklearn.cluster import KMeans 
 
 #socket
 context = zmq.Context()
@@ -28,10 +30,18 @@ for i, scan in enumerate(lidar.iter_scans()):
 	for cursor in scan:
 		if(((cursor[1]>329 and cursor[1]<361)or((cursor[1]>=0 and cursor[1]<31)))):
 			scan_FOV.append(cursor)
+			
 			#print(scan_FOV)
 			
 	if (len(scan_FOV)!=0):
-		socket.send(pickle.dumps(scan_FOV))
+		
+		np_scan= np.array(scan_FOV)
+		scan_kmeans=KMeans()
+		kmeans_scan_obj=scan_kmeans.fit(np_scan)
+		min_scan=kmeans_scan_obj.cluster_centers_
+		
+		print("Clustered Scan: ",min_scan)
+		socket.send(pickle.dumps(min_scan))
 
 		#  Get the reply.
 		message = socket.recv()
