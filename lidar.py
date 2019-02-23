21286b721b7ca7d189fc7d7c7688cdebe5752a19
ADAS-team <engr.shamoon@gmail.com>
2019-02-23 18:34:48 -0500

    1. Adder Radar distance displays (being sent in centimeters from fusion.py) 2. Increased radar/lidar circle thickness

--- lidar.py
+++ lidar.py
@@ -6,6 +6,26 @@ import numpy as np
 from sklearn.cluster import KMeans 
 import time
 
+
+
+def mkmeans(scan):
+	result=[]
+	length= len(scan)
+	i=0
+	temp_r=[]
+	temp_angle=[]
+	new_obj=True
+	for cursor in scan:
+		if(i>len-1):
+			break;
+		elif( (cursor-scan[i+1]<250) or (cursor-scan[i+1]<250) ):
+			if(new_obj):
+				temp_r.append()
+				temp_angle.append()
+		
+		else:
+			pass
+			
 #socket
 context = zmq.Context()
 print("Lidar Executable: Connecting to Fusion Engine…")
@@ -15,11 +35,10 @@ print("Connection Status:",socket.connect("tcp://localhost:5556"))
 #lidar initialization
 lidar = RPLidar('/dev/ttyUSB0')
 
+lidar.clear_input()
 
 
-
-
-lidar.reset()
+enable_kmeans=False
 
 #diagnostics
 while(True):
@@ -34,7 +53,8 @@ while(True):
 		scan_gen= lidar.iter_scans()
 		#print("Past iter_Scans")
 		
-
+		time.sleep(0)
+		
 		for i, scan in enumerate(scan_gen):
 			#print("I am in the loop")
 			
@@ -44,24 +64,28 @@ while(True):
 			n_clusters=10
 			
 			for cursor in scan:
-				if(((cursor[1]>329 and cursor[1]<361)or((cursor[1]>=0 and cursor[1]<31)))):
+				if(((cursor[1]>300 and cursor[1]<361)or((cursor[1]>=0 and cursor[1]<60)))):
 					scan_FOV.append(cursor)
 					
 					#print(scan_FOV)
 					
 			if (len(scan_FOV)!=0):
 				
-				np_scan= np.array(scan_FOV)
-				
-				while(len(scan_FOV)<n_clusters):
-					n_clusters= n_clusters-1
+				if(enable_kmeans):
 					
-				scan_kmeans=KMeans(n_clusters)
-				kmeans_scan_obj=scan_kmeans.fit(np_scan)
-				min_scan=kmeans_scan_obj.cluster_centers_
+					np_scan= np.array(scan_FOV)
+					
+					while(len(scan_FOV)<n_clusters):
+						n_clusters= n_clusters-1
+						
+					scan_kmeans=KMeans(n_clusters)
+					kmeans_scan_obj=scan_kmeans.fit(np_scan)
+					min_scan=kmeans_scan_obj.cluster_centers_
 				
-				#print("Clustered Scan: ",min_scan)
-				socket.send(pickle.dumps(min_scan))
+					#print("Clustered Scan: ",min_scan)
+					socket.send(pickle.dumps(min_scan))
+				else:
+					socket.send(pickle.dumps(scan_FOV))
 
 				#  Get the reply.
 				message = socket.recv()
--- mkmath.py
+++ mkmath.py
@@ -30,108 +30,8 @@ def convertToCartesanRadians(radius,radians):
     
 
 
-def computeOpticalCentre(FOV,f):
-    #expecting angle in degrees
-    return f*math.tan(FOV/2)
-
-
-"""def convertWorldCordsToPixels(U,V,W,f,FOVx,FOVy,RT,Sx,Sy):
-    #expecting RT  to be 4 x 4  numpy matrix
-    
-    UVW= np.array([[U],[V],[W],[1]]); #real world coords
-    per_proj= np.array([[f,0,0,0],[0,f,0,0],[0,0,1,0]]) #pespective projection
-    Ox= computeOpticalCentre(FOVx,f)
-    Oy= computeOpticalCentre(FOVy,f)
-    #Ox=800
-    #Oy=600
-    Maff= np.array([[1/Sx,0,Ox],[0,1/Sy,Oy],[0,0,1]])
-
-    #incomplete
-
-    print("RT X UVW")
-    print(RT.dot(UVW))
-    m= Maff.dot(per_proj)
-    print("Maff X per_proj")
-    print(m)
-    m= m.dot(RT)
-    print("Maff X per_proj X RT")
-    print(m)
-    m= m.dot(UVW)
-    return m"""
-
-#different math equation than the previous one
-def convertWorldCordsToPixels(U,V,W,f,FOVx,FOVy,RT,Sx,Sy):
-    #expecting RT  to be 4 x 4  numpy matrix
-    
-    UVW= np.array([[U],[V],[W],[1]]); #real world coords
-    
-    #Ox= computeOpticalCentre(FOVx,f)
-    #Oy= computeOpticalCentre(FOVy,f)
-    Ox=981
-    Oy=330
-    per_proj= np.array([[f/Sx,0,Ox,0],[0,f/Sy,Oy,0],[0,0,1,0]]) #pespective projection
-    
-
-    #incomplete
-    #print("RT X UVW")
-    m= RT.dot(UVW)
-    #print(m)
-    m= per_proj.dot(m)
-    #print("Per_proj X m")
-    #print(m)
-    return m
-
-
-def convertWorldCordsToPixelsCV(U,V,W,fx,fy,cx,cy,RT,Sx,Sy):
-    UVW= np.array([[U],[V],[W],[1]]);
-    intrinsic= np.array([[fx,0,cx],[0,fy,cy],[0,0,1]])
-    identity4Z= np.matrix('1 0 0 0;0 1 0 0;0 0 1 0')
-    s= np.array([[1/Sx,0,0],[0,1/Sy,0],[0,0,1]])
-    m= RT.dot(UVW)
-    #print(m)
-    z= intrinsic.dot(identity4Z)
-    m= z.dot(m)
-    #print(m)
-    m=s.dot(m)
-    #print(m)
-    return m
-
-def convertWorldCordsToPixelsDZ(U,V,W,fx,fy,cx,cy,RT,Sx,Sy):
-    UVW= np.array([[U/W],[V/W],[W/W],[1]]);
-    intrinsic= np.array([[fx,0,cx],[0,fy,cy],[0,0,1]])
-    identity4Z= np.matrix('1 0 0 0;0 1 0 0;0 0 1 0')
-    s= np.array([[1/Sx,0,0],[0,1/Sy,0],[0,0,1]])
-    m= RT.dot(UVW)
-    #print(m)
-    z= intrinsic.dot(identity4Z)
-    m= z.dot(m)
-    #print(m)
-    m=s.dot(m)
-    #print(m)
-    return m
-
-def convertWorldCordsToPixelsRadar(U,V,W,fx,fy,cx,cy,RT,Sx,Sy):
-    aug_z=0.7+W
-    UVW= np.array([[U/aug_z],[V/aug_z],[1],[1]]);
-    intrinsic= np.array([[fx,0,cx],[0,fy,cy],[0,0,1]])
-    identity4Z= np.matrix('1 0 0 0;0 1 0 0;0 0 1 0')
-    s= np.array([[1/Sx,0,0],[0,1/Sy,0],[0,0,1]])
-    m= RT.dot(UVW)
-    #print(m)
-    z= intrinsic.dot(identity4Z)
-    m= z.dot(m)
-    #print(m)
-    m=s.dot(m)
-    
-    #turning into homogenous coordinates
-    
-    
-    
-    #return list(int(m[0]), int(m[1]), int(m[2]))
-    return [int(m[0]), int(m[1]), int(m[2])]
   
-  
-"""Lidar""" 
+"""Lidar/Radar""" 
 
 def convertWithoutRT(U,fx,fy,cx,cy,Sx,Sy):
    
@@ -141,7 +41,6 @@ def convertWithoutRT(U,fx,fy,cx,cy,Sx,Sy):
 	if(m[2]==0):
 		m[2]=1
 		
-
 	m[0]=m[0]/m[2]
 	m[1]=m[1]/m[2]
 	m[2]=1
